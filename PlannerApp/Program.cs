3e5640f23a4cdef1e8769d6108148859a1b38be2
Sema T <sematumko@gmail.com>
2018-01-21 18:33:43 +0300

    Добавьте файлы проекта.

---
+++ PlannerApp/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TaskContext db = new TaskContext())
+            {
+                string query = "";
+
+                while (true)
+                {
+                    Console.Clear();
+
+                    db.showTasks();
+
+                    Console.WriteLine("\nДля отображения списка команд введите help\n");
+                    Console.Write("PlannerApp>");
+                    query = Console.ReadLine();
+
+
+                    if (query.ToLower().StartsWith("add "))
+                    {
+                        db.addTask(cutCommand(query));
+                    }
+                    else if (query.ToLower().StartsWith("upd "))
+                    {
+                        var tmpTask = db.findWithValidation(cutCommand(query));
+
+                        if (tmpTask != null)
+                        {
+                            Console.WriteLine("\nСТАРОЕ ЗНАЧЕНИЕ : {0}", tmpTask.Content);
+                            Console.Write("НОВОЕ ЗНАЧЕНИЕ  : ");
+                            string newValue = Console.ReadLine();
+
+                            db.updateTask(tmpTask, newValue);
+                        }
+                    }
+                    else if (query.ToLower().StartsWith("del "))
+                    {
+                        db.removeTask(cutCommand(query));
+                    }
+                    else if (query.Equals("clear", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        db.removeAllTasks();
+                    }
+
+                    else if (query.Equals("help", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Console.WriteLine("\nadd [задача]          Добавить задачу");
+                        Console.WriteLine("upd [id задачи]       Редактировать задачу");
+                        Console.WriteLine("del [id задачи]       Удалить задачу");
+                        Console.WriteLine("clear                 Удалить все задачи");
+                        Console.WriteLine("help                  Список команд");
+                        Console.WriteLine("exit                  Выход");
+
+                        Console.WriteLine("\nНажмите любую клавишу, чтобы продолжить...");
+                        Console.ReadKey();
+                    }
+                    else if (query.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Console.WriteLine("\nBye");
+                        System.Threading.Thread.Sleep(300);
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nНеизвестная команда или формат. Для получения списка команд введите help");
+                        Console.ReadKey();
+                    }
+                }
+            }
+        }
+
+        // отсекает команду
+        public static string cutCommand(string _query) => _query.Remove(0, _query.IndexOf(" ") + 1);
+    }
+}
---
+++ PlannerApp/TaskContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace PlannerApp
+{
+    class TaskContext : DbContext
+    {
+        public TaskContext()
+            : base("DbConnection")
+        { }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public void addTask(string _queryBody)
+        {
+            _queryBody = _queryBody.Trim(' ', '\t');
+
+            if (_queryBody.Length > 0)
+            {
+                Tasks.Add(new Task { Content = _queryBody });
+                this.SaveChanges();
+            }
+        }
+
+        public void updateTask(Task _task, string _newValue)
+        {
+            _newValue = _newValue.Trim(' ', '\t');
+
+            if (_newValue.Length > 0)
+            {
+                _task.Content = _newValue;
+                this.SaveChanges();
+            }
+        }
+
+        public void removeTask(string _queryBody)
+        {
+            _queryBody = _queryBody.Trim(' ', '\t');
+
+            int tmpId = -1;
+
+            if (Int32.TryParse(_queryBody, out tmpId))
+                if (tmpId > 0 && Tasks.Find(tmpId) != null)
+                {
+                    Tasks.Remove(Tasks.Find(tmpId));
+                    this.SaveChanges();
+                }
+        }
+
+        public void removeAllTasks()
+        {
+            this.Tasks.RemoveRange(this.Tasks);
+            this.SaveChanges();
+        }
+
+        public Task findWithValidation(string _queryBody)
+        {
+            _queryBody = _queryBody.Trim(' ', '\t');
+
+            int tmpId = -1;
+
+            if (Int32.TryParse(_queryBody, out tmpId))
+                if (tmpId > 0)
+                    return Tasks.Find(tmpId);
+
+            return null;
+        }
+
+        public void showTasks()
+        {
+            foreach (Task tsk in this.Tasks)
+            {
+                Console.WriteLine("{0}. {1}", tsk.Id, tsk.Content);
+            }
+        }
+    }
+}
